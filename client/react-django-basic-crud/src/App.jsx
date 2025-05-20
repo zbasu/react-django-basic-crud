fb207888baf2ffb98d70f0350c2f3673deeb39d6
zbasu <doukuna@gmail.com>
2025-05-19 20:24:33 -0400

    Put together CRUD functionality
    Set up API calls and linked them to button events

--- client/react-django-basic-crud/src/App.jsx
+++ client/react-django-basic-crud/src/App.jsx
@@ -1,35 +1,123 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [books, setBooks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [releaseYear, setReleaseYear] = useState(0);
+
+  const [newTitle, setNewTitle] = useState("");
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/");
+      const data = await response.json();
+      setBooks(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addBook = async () => {
+    const bookData = {
+      title,
+      release_year: releaseYear,
+    };
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/create/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bookData),
+      });
+
+      const data = await response.json();
+      setBooks((prev) => [...prev, data]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const updateTitle = async (pk, release_year) => {
+    const bookData = {
+      title: newTitle,
+      release_year,
+    };
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/books/${pk}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bookData),
+      });
+
+      const data = await response.json();
+      setBooks((prev) =>
+        prev.map((book) => {
+          if (book.id === pk) {
+            return data;
+          } else {
+            return book;
+          }
+        })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteBook = async (pk) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/books/${pk}`, {
+        method: "DELETE",
+      });
+
+      setBooks((prev) => prev.filter((book) => book.id !== pk));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
+      <h1> Book Website </h1>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input
+          type="text"
+          placeholder="Book Title..."
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Release Year..."
+          onChange={(e) => setReleaseYear(e.target.value)}
+        />
+        <button onClick={addBook}> Add Book </button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {books.map((book) => (
+        <div>
+          <p>Title: {book.title}</p>
+          <p>Release Year: {book.release_year} </p>
+          <input
+            type="text"
+            placeholder="New Title..."
+            onChange={(e) => setNewTitle(e.target.value)}
+          />
+          <button onClick={() => updateTitle(book.id, book.release_year)}>
+            Change Title
+          </button>
+          <button onClick={() => deleteBook(book.id)}> Delete</button>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
